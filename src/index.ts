a05d36e605ff44dbeb85f478c41bf9fd791db401
Lennart Gastler <lennart@lgastler.de>
2022-01-01 22:20:15 +0100

    feat(init): added initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,77 @@
-import { SessionStorage } from "@remix-run/server-runtime";
+// We need to import from Remix Auth the type of the strategy verify callback
+import type { StrategyVerifyCallback } from "remix-auth";
+// We need to import the OAuth2Strategy, the verify callback and the profile interfaces
 import {
-  AuthenticateOptions,
-  Strategy,
-  StrategyVerifyCallback,
-} from "remix-auth";
+  OAuth2Profile,
+  OAuth2Strategy,
+  OAuth2StrategyVerifyParams,
+} from "remix-auth-oauth2";
 
-/**
- * This interface declares what configuration the strategy needs from the
- * developer to correctly work.
- */
-export interface MyStrategyOptions {
-  something: "You may need";
+// These are the custom options we need from the developer to use the strategy
+export interface AppleStrategyOptions {
+  clientID: string;
+  clientSecret: string;
+  callbackURL: string;
 }
 
-/**
- * This interface declares what the developer will receive from the strategy
- * to verify the user identity in their system.
- */
-export interface MyStrategyVerifyParams {
-  something: "Dev may need";
+// These interface declare what extra params we will get from Apple on the
+// verify callback
+export interface AppleExtraParams extends Record<string, string | number> {
+  id_token: string;
+  expires_in: 3600;
+  token_type: "Bearer";
 }
 
-export class MyStrategy<User> extends Strategy<User, MyStrategyVerifyParams> {
-  name = "change-me";
+// The AppleProfile extends the OAuth2Profile with the extra params and mark
+// some of them as required
+export type AppleProfile = OAuth2Profile;
 
+// And we create our strategy extending the OAuth2Strategy, we also need to
+// pass the User as we did on the FormStrategy, we pass the Auth0Profile and the
+// extra params
+export class AppleStrategy<User> extends OAuth2Strategy<
+  User,
+  AppleProfile,
+  AppleExtraParams
+> {
+  // The OAuth2Strategy already has a name but we can override it
+  name = "apple";
+
+  // We receive our custom options and our verify callback
   constructor(
-    options: MyStrategyOptions,
-    verify: StrategyVerifyCallback<User, MyStrategyVerifyParams>
+    options: AppleStrategyOptions,
+    verify: StrategyVerifyCallback<
+      User,
+      OAuth2StrategyVerifyParams<AppleProfile, AppleExtraParams>
+    >
   ) {
-    super(verify);
-    // do something with the options here
+    // And we pass the options to the super constructor using our own options
+    // to generate them, this was we can ask less configuration to the developer
+    // using our strategy
+
+    super(
+      {
+        authorizationURL: `https://appleid.apple.com/auth/authorize`,
+        tokenURL: `https://appleid.apple.com/auth/token`,
+        clientID: options.clientID,
+        clientSecret: options.clientSecret,
+        callbackURL: options.callbackURL,
+      },
+      verify
+    );
   }
 
-  async authenticate(
-    request: Request,
-    sessionStorage: SessionStorage,
-    options: AuthenticateOptions
-  ): Promise<User> {
-    return await this.failure(
-      "Implement me!",
-      request,
-      sessionStorage,
-      options
-    );
-    // Uncomment me to do a success response
-    // this.success({} as User, request, sessionStorage, options);
+  protected async userProfile(): Promise<AppleProfile> {
+    return { provider: "apple" };
+  }
+
+  // We override the protected authorizationParams method to return a new
+  // URLSearchParams with custom params we want to send to the authorizationURL.
+  // Here we add the scope so Auth0 can use it, you can pass any extra param
+  // you need to send to the authorizationURL here base on your provider.
+  protected authorizationParams() {
+    return new URLSearchParams({
+      response_mode: "query",
+    });
   }
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,9 @@
 import { createCookieSessionStorage } from "@remix-run/server-runtime";
-import { MyStrategy } from "../src";
 
-describe(MyStrategy, () => {
+import { AppleStrategy } from "../src";
+
+describe(AppleStrategy, () => {
   let verify = jest.fn();
-  // You will probably need a sessionStorage to test the strategy.
   let sessionStorage = createCookieSessionStorage({
     cookie: { secrets: ["s3cr3t"] },
   });
@@ -12,10 +12,89 @@ describe(MyStrategy, () => {
     jest.resetAllMocks();
   });
 
-  test("should have the name of the strategy", () => {
-    let strategy = new MyStrategy({ something: "You may need" }, verify);
-    expect(strategy.name).toBe("change-me");
+  test("should have no scope specified", async () => {
+    let strategy = new AppleStrategy(
+      {
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/apple");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.searchParams.get("scope")).toBe(null);
+    }
   });
 
-  test.todo("Write more tests to check everything works as expected");
+  test("should have response mode query", async () => {
+    let strategy = new AppleStrategy(
+      {
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/apple");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.searchParams.get("response_mode")).toBe("query");
+    }
+  });
+
+  test("should correctly format the authorization URL", async () => {
+    let strategy = new AppleStrategy(
+      {
+        clientID: "CLIENT_ID",
+        clientSecret: "CLIENT_SECRET",
+        callbackURL: "https://example.app/callback",
+      },
+      verify
+    );
+
+    let request = new Request("https://example.app/auth/apple");
+
+    try {
+      await strategy.authenticate(request, sessionStorage, {
+        sessionKey: "user",
+      });
+    } catch (error) {
+      if (!(error instanceof Response)) throw error;
+
+      let location = error.headers.get("Location");
+
+      if (!location) throw new Error("No redirect header");
+
+      let redirectUrl = new URL(location);
+
+      expect(redirectUrl.hostname).toBe("appleid.apple.com");
+      expect(redirectUrl.pathname).toBe("/auth/authorize");
+    }
+  });
 });
